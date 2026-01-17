3fc4da80578e366643d921b40c1f880df4d8bb6d
ilker09091 <ilker09091@gmail.com>
2026-01-17 20:20:15 +0400

    created by ilkerdeveloper

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Output from "./pages/Output";
@@ -10,6 +10,20 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search-dunyanin-en-guzel-kizi" element={<Output />} />
       </Routes>
+
+      <div className="absolute bottom-0 w-full bg-gray-200">
+        <p className="text-center">
+          created by{" "}
+          <a
+            href="https://github.com/ilker09091"
+            target="_blank"
+            rel="noopener noreferrer "
+            className="text-bold "
+          >
+            ilkerdeveloper
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
